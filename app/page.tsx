715c03fced262bd95a0ac38a9aec851111da1f6c
FelipePetriw <felipe.petriw.ads@gmail.com>
2024-08-06 19:05:58 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import { Button } from "./_components/ui/button"
 import Header from "./_components/ui/header"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
@@ -21,9 +24,34 @@ const Home = () => {
           </Button>
         </div>
 
-        <div className="relative w-full h-[400px] mt-6">
+        {/* Banner */}
+        <div className="relative w-full h-[150px] mt-6">
           <Image alt="Banner" src="/banner-01.png" fill className="object-cover rounded-xl"/>
         </div>
+
+        {/* Agendamentos */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            {/* Campo Status do Agendamento */}
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/5832df58-cfd7-4b3f-b102-42b7e150ced2-16r.png"/>
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+            {/* Data do Agendamento */}
+            <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
