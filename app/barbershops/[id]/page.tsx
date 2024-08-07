222795a60128137a7ec0f86ea709c1c9d51eeb53
FelipePetriw <felipe.petriw.ads@gmail.com>
2024-08-07 20:59:48 -0300

    feat: add initial barbershop page

---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,71 @@
+import { Button } from "@/app/_components/ui/button";
+import { db } from "@/app/_lib/prisma";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+interface BarbershopPageProps {
+    params: {
+        id: string
+    }
+}
+
+const BarbershopPage = async ({params}: BarbershopPageProps) => {
+
+    //Chamar o banco de dados
+    const barbershop = await db.barbershop.findUnique({
+        where: {
+            id: params.id,
+        },
+    })
+
+    if (!barbershop) {
+        return notFound()
+    }
+
+    return ( 
+        <div>
+            {/* Imagem de Capa Barbearia */}
+            <div className="relative h-[250px] w-full">
+            <Image alt={barbershop.name} src={barbershop?.imageUrl} fill className="object-cover" />
+            
+            {/* Seta de Voltar */}
+            <Button size="icon" variant="secondary" className="absolute top-4 left-4" asChild>
+                <Link href="/">
+                    <ChevronLeftIcon />
+                </Link>
+            </Button>
+
+            {/* Menu Lateral */}
+            <Button size="icon" variant="secondary" className="absolute top-4 right-4">
+                <MenuIcon />
+            </Button>
+        </div>
+
+        <div className="border-b border-solid p-5">
+            <h1 className="mb-3 text-xl font-bold">{barbershop.name}</h1>
+            
+            {/* Localização */}
+            <div className="mb-2 flex items-center gap-2">
+                <MapPinIcon className="text-primary" size={18}/>
+                <p className="text-sm">{barbershop?.address}</p>
+            </div>
+
+            {/* Avaliações */}
+            <div className="mb-2 flex items-center gap-2">
+                <StarIcon className="fill-primary text-primary" size={18}/>
+                <p className="text-sm">5,0 (499 avaliações)</p>
+            </div>
+        </div>
+
+        {/* Descrição */}
+        <div className="p-5 border-b border-solid space-y-3">
+            <h2 className="text-xs font-bold uppercase text-gray-400">Sobre Nós</h2>
+            <p className="text-sm text-justify">{barbershop?.description}</p>
+        </div>
+        </div>
+    );
+}
+
+export default BarbershopPage;
