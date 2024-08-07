2973259c82645ade2def1e2ed211d925b5cae18a
FelipePetriw <felipe.petriw.ads@gmail.com>
2024-08-07 15:31:48 -0300

    feat: ass quick search

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,11 @@ import BarbershopItem from "./_components/barbershop-item"
 const Home = async () => {
   // Chamar meu Banco de Dados
   const barbershops = await db.barbershop.findMany({})
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    }
+  })
   
   return (
     <div>
@@ -23,9 +28,42 @@ const Home = async () => {
 
         {/* Barra e Botão de Pesquisa */}
         <div className="flex items-center gap-2 mt-6">
-          <Input placeholder="Pesquisar"/>
+          <Input placeholder="Buscar"/>
           <Button>
             <SearchIcon/>
+          </Button>
+        </div>
+
+        {/* Busca Rápida */}
+        <div className="flex gap-3 mt-6 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          <Button className="gap-2" variant="secondary">
+            <Image src="./cabelo.svg" width={16} height={16} alt="Cabelo"/>
+            Cabelo
+          </Button>
+
+          <Button className="gap-2" variant="secondary">
+            <Image src="./barba.svg" width={16} height={16} alt="Barba"/>
+            Barba
+          </Button>
+
+          <Button className="gap-2" variant="secondary">
+            <Image src="./acabamento.svg" width={16} height={16} alt="Acabamento"/>
+            Acabamento
+          </Button>
+
+          <Button className="gap-2" variant="secondary">
+            <Image src="./hidratacao.svg" width={16} height={16} alt="Hidratação"/>
+            Hidratação
+          </Button>
+
+          <Button className="gap-2" variant="secondary">
+            <Image src="./massagem.svg" width={16} height={16} alt="Massagem"/>
+            Massagem
+          </Button>
+
+          <Button className="gap-2" variant="secondary">
+            <Image src="./sobrancelha.svg" width={16} height={16} alt="Sobrancelha"/>
+            Sobrancelha
           </Button>
         </div>
 
@@ -70,7 +108,24 @@ const Home = async () => {
         <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (<BarbershopItem key={barbershop.id} barbershop={barbershop} />))}
         </div>
+
+        {/* Populares */}
+        <h2 className="uppercase text-gray-400 font-bold text-xs mt-6 mb-3">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((barbershop) => (<BarbershopItem key={barbershop.id} barbershop={barbershop} />))}
+        </div>
       </div>
+
+      <footer>
+        <Card>
+          <CardContent className="px-5 py-6">
+            <p className="text-sm text-gray-400">© 2023 Copyright <span className="font-bold">FSW Barber</span></p>
+          </CardContent>
+        </Card>
+      </footer>
+
     </div>
   )
 }
