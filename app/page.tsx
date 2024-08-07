2408745b3e0b566eaa0832cfd55ab4d4b8a4aead
FelipePetriw <felipe.petriw.ads@gmail.com>
2024-08-07 13:49:15 -0300

    feat: add BarbershopItem component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 import { SearchIcon } from "lucide-react"
 import { Button } from "./_components/ui/button"
-import Header from "./_components/ui/header"
+import Header from "./_components/header"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
 import { Card, CardContent } from "./_components/ui/card"
 import { Badge } from "./_components/ui/badge"
 import { Avatar, AvatarImage } from "./_components/ui/avatar"
+import { db } from "./_lib/prisma"
+import BarbershopItem from "./_components/barbershop-item"
 
-const Home = () => {
+const Home = async () => {
+  // Chamar meu Banco de Dados
+  const barbershops = await db.barbershop.findMany({})
+  
   return (
     <div>
       {/*Header*/}
@@ -30,7 +35,11 @@ const Home = () => {
         </div>
 
         {/* Agendamentos */}
-        <Card className="mt-6">
+        <h2 className="uppercase text-gray-400 font-bold text-xs mt-6 mb-3">
+          Agendamentos
+        </h2>
+
+        <Card>
           <CardContent className="flex justify-between p-0">
             {/* Campo Status do Agendamento */}
             <div className="flex flex-col gap-2 py-5 pl-5">
@@ -44,6 +53,7 @@ const Home = () => {
                 <p className="text-sm">Barbearia FSW</p>
               </div>
             </div>
+
             {/* Data do Agendamento */}
             <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
               <p className="text-sm">Agosto</p>
@@ -52,6 +62,14 @@ const Home = () => {
             </div>
           </CardContent>
         </Card>
+
+        {/* Recomendados */}
+        <h2 className="uppercase text-gray-400 font-bold text-xs mt-6 mb-3">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (<BarbershopItem key={barbershop.id} barbershop={barbershop} />))}
+        </div>
       </div>
     </div>
   )
