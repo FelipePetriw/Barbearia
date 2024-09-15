9b45fcca57592f7ef3c9aa010a635da13c5c3f2a
FelipePetriw <felipe.petriw.ads@gmail.com>
2024-09-15 12:10:15 -0300

    feat: update welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,8 @@ import Search from "./_components/search"
 import Link from "next/link"
 import { getServerSession } from "next-auth"
 import { authOptions } from "./_lib/auth"
+import { ptBR } from "date-fns/locale"
+import { format } from "date-fns"
 
 const Home = async () => {
   // Chamar meu Banco de Dados
@@ -43,8 +45,10 @@ const Home = async () => {
       {/*Header*/}
       <Header />
       <div className="p-5">
-        <h2 className="text-xl font-bold">Olá, Felipe!</h2>
-        <p>Segunda-feira, 05 de agosto.</p>
+        <h2 className="text-xl font-bold">Olá, {session?.user ? session.user.name : "bem vindo"}!</h2>
+        <span className="capitalize">{format(new Date(), "EEEE, dd ", { locale: ptBR })}</span>
+        <span>de</span>
+        <span className="capitalize">{format(new Date(), " MMMM", { locale: ptBR })}</span>
 
         {/* Barra e Botão de Pesquisa */}
         <div className="mt-6">
